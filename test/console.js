0d284c223d18cd515b9efb29a2e49e48f73b2a72
Jeff Haynie <jhaynie@appcelerator.com>
2014-09-15 08:45:45 -0700

    fixed color coding and added unit tests

--- test/console.js
+++ test/console.js
@@ -1,5 +1,6 @@
 var should = require('should'),
-	_console = new (require('./_console'))(),
+	ConsoleClass = require('./_console'),
+	_console = new ConsoleClass(),
 	index = require('../'),
 	ConsoleLogger = index.ConsoleLogger;
 
@@ -143,7 +144,6 @@ describe("console", function(){
 	});
 
 	it("should be able to use format symbols", function(callback){
-		should(ConsoleLogger).be.an.object;
 		try {
 			_console.start();
 			_console.on('data',function(buf){
@@ -158,6 +158,29 @@ describe("console", function(){
 		}
 		finally {
 			_console.stop();
+		}
+	});
+
+	it("should remove color coding", function(callback){
+		var console_ = new ConsoleClass(false);
+		var travis = process.env.TRAVIS;
+		try {
+			console_.start();
+			console_.on('data',function(buf){
+				console_.stop();
+				should(buf).equal('INFO   | hello world 1');
+				callback();
+			});
+			var logger = index.createDefaultLogger();
+			should(logger).be.an.object;
+			should(logger.info).be.a.function;
+			var chalk = require('chalk');
+			process.env.TRAVIS = 1; // force log coloring off
+			logger.info('hello %s %d',chalk.red('world'),1);
+		}
+		finally {
+			process.env.TRAVIS = travis; // reset travis setting
+			console_.stop();
 		}
 	});
 });
